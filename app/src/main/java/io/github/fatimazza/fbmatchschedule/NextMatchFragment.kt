c5a5eb2cda2465d1613ad093f6c17585447396af
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-24 12:32:31 +0700

    Main Fragment - Add new Fragment for Last Match and Next Match

---
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/NextMatchFragment.kt
@@ -0,0 +1,15 @@
+package io.github.fatimazza.fbmatchschedule
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NextMatchFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+            : View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
