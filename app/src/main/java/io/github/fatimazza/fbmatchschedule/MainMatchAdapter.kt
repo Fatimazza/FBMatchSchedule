16ac88d2b623980d5c4300577f91acc725e5b333
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-25 06:43:52 +0700

    Main Match Adapter - Add Layout for Item of RecyclerView, Show it on onCreate

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/MainMatchAdapter.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/MainMatchAdapter.kt
@@ -4,12 +4,17 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import io.github.fatimazza.fbmatchschedule.model.Event
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.linearLayout
 
 class MainMatchAdapter(private val events: List<Event>)
     : RecyclerView.Adapter<EventViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return EventViewHolder(EventUI().createView(
+                AnkoContext.create(parent.context, parent)
+        ))
     }
 
     override fun getItemCount(): Int {
@@ -20,6 +25,14 @@ class MainMatchAdapter(private val events: List<Event>)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+}
+
+class EventUI(): AnkoComponent<ViewGroup> {
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui) {
+            linearLayout {  }
+        }
+    }
 }
 
 class EventViewHolder(view: View): RecyclerView.ViewHolder(view) {
