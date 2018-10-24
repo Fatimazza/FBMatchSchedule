3bac44074666a96acc130bcf640c10a2039a4c66
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-25 06:23:24 +0700

    Main Match Adapter - Add new Adapter for Recyclerview

---
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/MainMatchAdapter.kt
@@ -0,0 +1,17 @@
+package io.github.fatimazza.fbmatchschedule
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import io.github.fatimazza.fbmatchschedule.model.Event
+
+class MainMatchAdapter(private val events: List<Event>) {
+
+}
+
+class EventViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    fun bindItem(events: Event) {
+
+    }
+
+}
