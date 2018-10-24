7eb36e2bd2ee526c4fb004f223c74a35118e6c17
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-24 11:59:58 +0700

    Main Tab - Add Pager Adapter for Tab

---
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/MainPagerAdapter.kt
@@ -0,0 +1,17 @@
+package io.github.fatimazza.fbmatchschedule
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MainPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
