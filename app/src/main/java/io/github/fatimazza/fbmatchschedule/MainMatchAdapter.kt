cd64a3cfd823361f602976ac87827fc4feef11d3
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-25 06:26:02 +0700

    Main Match Adapter - Make Adapter implement ViewHolder

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/MainMatchAdapter.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/MainMatchAdapter.kt
@@ -2,9 +2,23 @@ package io.github.fatimazza.fbmatchschedule
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 import io.github.fatimazza.fbmatchschedule.model.Event
 
-class MainMatchAdapter(private val events: List<Event>) {
+class MainMatchAdapter(private val events: List<Event>)
+    : RecyclerView.Adapter<EventViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
 
