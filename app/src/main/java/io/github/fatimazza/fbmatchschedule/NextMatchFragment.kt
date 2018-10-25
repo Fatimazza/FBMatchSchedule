4090d17ba625658131e0872159298e9d4fce44ed
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-25 10:00:36 +0700

    Networking, Next Match Fragment - Request data from Server, Load data

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/NextMatchFragment.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/NextMatchFragment.kt
@@ -7,7 +7,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.gson.Gson
 import io.github.fatimazza.fbmatchschedule.model.Event
+import io.github.fatimazza.fbmatchschedule.model.EventResponse
+import io.github.fatimazza.fbmatchschedule.network.ApiRepository
+import io.github.fatimazza.fbmatchschedule.network.TheSportDBApi
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.ctx
@@ -40,10 +44,31 @@ class NextMatchFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
+        requestData()
     }
 
     private fun initAdapter() {
         adapter = MainMatchAdapter(events)
         listNextEvent.adapter = adapter
+    }
+
+    private fun requestData() {
+        val request = ApiRepository()
+        val gson = Gson()
+        //doInPresenter
+        doAsync {
+            val data = gson.fromJson(request.doRequest(
+                    TheSportDBApi.getNextMatch()),
+                    EventResponse::class.java)
+            uiThread {
+                showEventList(data.events)
+            }
+        }
+    }
+
+    private fun showEventList(data: List<Event>) {
+        events.clear()
+        events.addAll(data)
+        adapter.notifyDataSetChanged()
     }
 }
