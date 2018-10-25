c8fcb57945659f6bb70940ef825b9935e95b982f
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-25 07:11:30 +0700

    Main Adapter, Resources - Add Views to RecyclerView Item Layout, Add Id Resources

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/MainMatchAdapter.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/MainMatchAdapter.kt
@@ -1,12 +1,12 @@
 package io.github.fatimazza.fbmatchschedule
 
 import android.support.v7.widget.RecyclerView
+import android.view.Gravity.*
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import io.github.fatimazza.fbmatchschedule.model.Event
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.linearLayout
+import org.jetbrains.anko.*
 
 class MainMatchAdapter(private val events: List<Event>)
     : RecyclerView.Adapter<EventViewHolder>() {
@@ -30,7 +30,53 @@ class MainMatchAdapter(private val events: List<Event>)
 class EventUI(): AnkoComponent<ViewGroup> {
     override fun createView(ui: AnkoContext<ViewGroup>): View {
         return with(ui) {
-            linearLayout {  }
+            linearLayout {
+                lparams(width = matchParent, height = wrapContent)
+                padding = dip(16)
+                orientation = LinearLayout.VERTICAL
+                gravity = CENTER
+
+                textView {
+                    id = R.id.match_date
+                    textSize = 14f
+                    text = "Sat, 22 Sep 2008"
+                }.lparams{
+                    margin = dip(12)
+                }
+
+                linearLayout {
+                    lparams(width = matchParent, height = wrapContent)
+                    weightSum = 3f
+                    orientation = LinearLayout.HORIZONTAL
+
+                    textView {
+                        id = R.id.home_team_name
+                        text = "Team A"
+                        textSize = 16f
+                        gravity = RIGHT
+                    }.lparams{
+                        weight = 1f
+                    }
+
+                    textView {
+                        id = R.id.match_score
+                        text = "1 vs 1"
+                        textSize = 16f
+                        gravity = CENTER
+                    }.lparams{
+                        weight = 1f
+                    }
+
+                    textView {
+                        id = R.id.away_team_name
+                        text = "Team B"
+                        textSize = 16f
+                        gravity = LEFT
+                    }.lparams{
+                        weight = 1f
+                    }
+                }
+            }
         }
     }
 }
