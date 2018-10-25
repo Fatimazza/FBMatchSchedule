685c8e9b722e0c47d51ba7a19e84619308bdefba
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-25 09:49:48 +0700

    Next Match Fragment - Add layout containing RecyclerView

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/NextMatchFragment.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/NextMatchFragment.kt
@@ -2,15 +2,33 @@ package io.github.fatimazza.fbmatchschedule
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.ctx
 
 class NextMatchFragment: Fragment() {
 
+    private lateinit var listNextEvent: RecyclerView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
             : View? {
-        return inflater
-                .inflate(R.layout.fragment_next_match, container, false)
+        return NextMatchFragmentUI().createView(
+                AnkoContext.create(ctx, this, false))
+    }
+
+    class NextMatchFragmentUI: AnkoComponent<NextMatchFragment> {
+        override fun createView(ui: AnkoContext<NextMatchFragment>): View = with(ui) {
+            frameLayout {
+                owner.listNextEvent = recyclerView {
+                    lparams(width = matchParent, height = wrapContent)
+                    layoutManager = LinearLayoutManager(ctx)
+                }
+            }
+        }
     }
 }
