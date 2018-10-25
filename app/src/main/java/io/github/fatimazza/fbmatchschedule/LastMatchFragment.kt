700959c54e859e94c001c832a8dd5fe639f4c3b4
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-25 08:22:47 +0700

    Last Match Fragment - Add layout containing RecyclerView

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/LastMatchFragment.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/LastMatchFragment.kt
@@ -2,15 +2,30 @@ package io.github.fatimazza.fbmatchschedule
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.anko.frameLayout
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.UI
+import org.jetbrains.anko.wrapContent
 
 class LastMatchFragment: Fragment() {
 
+    private lateinit var listEvent: RecyclerView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
             : View? {
-        return inflater
-                .inflate(R.layout.fragment_last_match, container, false)
+        return UI {
+            frameLayout {
+                listEvent = recyclerView {
+                    lparams(width = matchParent, height = wrapContent)
+                    layoutManager = LinearLayoutManager(ctx)
+                }
+            }
+        }.view
     }
 }
