4b54d72a4f2268d49ede378d8c2d3c2e613bd1e0
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-29 13:36:38 +0700

    Match Detail Layout - Change to XML layout

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/matchdetail/MatchDetailActivity.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/matchdetail/MatchDetailActivity.kt
@@ -2,29 +2,14 @@ package io.github.fatimazza.fbmatchschedule.matchdetail
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import io.github.fatimazza.fbmatchschedule.R
-import org.jetbrains.anko.*
 
 class MatchDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MatchDetailUI().setContentView(this)
+        setContentView(R.layout.activity_detail)
+
     }
 
-    class MatchDetailUI: AnkoComponent<MatchDetailActivity> {
-        override fun createView(ui: AnkoContext<MatchDetailActivity>): View {
-            return with(ui) {
-                linearLayout {
-                    lparams(width = matchParent, height = matchParent)
-                    padding = dip(16)
-
-                    view {
-                        backgroundColor = resources.getColor(R.color.colorAccent)
-                    }.lparams(width = matchParent, height = dip(1))
-                }
-            }
-        }
-    }
 }
