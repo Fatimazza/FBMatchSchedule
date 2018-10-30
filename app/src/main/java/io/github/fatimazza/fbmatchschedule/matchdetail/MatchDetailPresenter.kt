10e20e11ce2b9450180b164820224deeaf101b0b
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-30 13:00:00 +0700

    Match Detail MVP - Add Presenter

---
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/matchdetail/MatchDetailPresenter.kt
@@ -0,0 +1,37 @@
+package io.github.fatimazza.fbmatchschedule.matchdetail
+
+import com.google.gson.Gson
+import io.github.fatimazza.fbmatchschedule.model.TeamResponse
+import io.github.fatimazza.fbmatchschedule.network.ApiRepository
+import io.github.fatimazza.fbmatchschedule.network.TheSportDBApi
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class MatchDetailPresenter(private val view: MatchDetailView,
+                           private val apiRepository: ApiRepository,
+                           private val gson: Gson) {
+
+    fun getTeamDetail(idHomeTeam: String, idAwayTeam: String) {
+        
+        doAsync {
+            val data = gson.fromJson(
+                    apiRepository.doRequest(TheSportDBApi.getTeamDetail(idHomeTeam)),
+                    TeamResponse::class.java
+            )
+            uiThread {
+                view.showHomeTeamDetail(data.team)
+            }
+        }
+
+        doAsync {
+            val data = gson.fromJson(
+                    apiRepository.doRequest(TheSportDBApi.getTeamDetail(idAwayTeam)),
+                    TeamResponse::class.java
+            )
+            uiThread {
+                view.showAwayTeamDetail(data.team)
+            }
+        }
+    }
+
+}
