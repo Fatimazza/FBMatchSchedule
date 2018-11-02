ba1722fd21ef5ef90eeec272fc37e28a123dcf35
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-02 17:12:52 +0700

    Main Fragments - Load Fragment when Bottom Navigation is selected

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/main/MainActivity.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/main/MainActivity.kt
@@ -2,7 +2,11 @@ package io.github.fatimazza.fbmatchschedule.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import io.github.fatimazza.fbmatchschedule.R
+import io.github.fatimazza.fbmatchschedule.R.id.*
+import io.github.fatimazza.fbmatchschedule.lastmatch.LastMatchFragment
+import io.github.fatimazza.fbmatchschedule.nextmatch.NextMatchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentAdapter = MainPagerAdapter(supportFragmentManager, this)
-        viewpager_main.adapter = fragmentAdapter
-        tabs_main.setupWithViewPager(viewpager_main)
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            item -> when(item.itemId) {
+                last_match -> { loadFragment(savedInstanceState, LastMatchFragment()) }
+                next_match -> { loadFragment(savedInstanceState, NextMatchFragment()) }
+                favorite_match -> { loadFragment(savedInstanceState, LastMatchFragment()) }
+            }
+            true
+        }
+        bottom_navigation.selectedItemId = last_match
     }
+
+    private fun loadFragment(savedInstanceState: Bundle?, fragment: Fragment) {
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, fragment,
+                            fragment::class.java.simpleName)
+                    .commit()
+        }
+    }
+
 }
