72273237c82c248fee748013cb2ae510627a2ca0
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-02 17:37:55 +0700

    Favorite Fragment - Add Anko Layout

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/favoritematch/FavoriteMatchFragment.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/favoritematch/FavoriteMatchFragment.kt
@@ -2,17 +2,23 @@ package io.github.fatimazza.fbmatchschedule.favoritematch
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.frameLayout
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.ctx
 
 class FavoriteMatchFragment: Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    private lateinit var listFavEvent: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+            : View? {
+        return FavoriteMatchUI().createView(
+                AnkoContext.Companion.create(ctx, this, false))
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,7 +30,10 @@ class FavoriteMatchFragment: Fragment() {
         override fun createView(ui: AnkoContext<FavoriteMatchFragment>): View {
             return with(ui) {
                 frameLayout {
-
+                    owner.listFavEvent = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
                 }
             }
         }
