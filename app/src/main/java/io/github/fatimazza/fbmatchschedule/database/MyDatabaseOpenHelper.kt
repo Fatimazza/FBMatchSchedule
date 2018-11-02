dd0d1bc13cf5d9bd823eb3cb027b5950a9c1750d
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-02 18:49:26 +0700

    Database - In DB Open Helper implements all functions, Add function to get Database instance

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/database/MyDatabaseOpenHelper.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/database/MyDatabaseOpenHelper.kt
@@ -2,18 +2,45 @@ package io.github.fatimazza.fbmatchschedule.database
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+import org.jetbrains.anko.db.*
 
 class MyDatabaseOpenHelper(ctx: Context)
     : ManagedSQLiteOpenHelper(ctx, "FavoriteEvents.db", null, 1) {
 
-    override fun onCreate(db: SQLiteDatabase?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    companion object {
+
+        private var instance: MyDatabaseOpenHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): MyDatabaseOpenHelper {
+            if (instance == null) {
+                instance = MyDatabaseOpenHelper(ctx.applicationContext)
+            }
+            return instance as MyDatabaseOpenHelper
+        }
+        
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreate(db: SQLiteDatabase) {
+        // create Tables
+        db.createTable(FavoriteMatch.TABLE_FAVORITE, true,
+                FavoriteMatch.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                FavoriteMatch.EVENT_ID to TEXT + UNIQUE,
+                FavoriteMatch.EVENT_DATE to TEXT,
+                FavoriteMatch.HOME_TEAM to TEXT,
+                FavoriteMatch.AWAY_TEAM to TEXT,
+                FavoriteMatch.HOME_SCORE to TEXT,
+                FavoriteMatch.AWAY_SCORE to TEXT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        // upgrade Tables
+        db.dropTable(FavoriteMatch.TABLE_FAVORITE, true)
     }
 
 }
+
+// Access property for Context
+val Context.database: MyDatabaseOpenHelper
+    get() = MyDatabaseOpenHelper.getInstance(applicationContext)
 
