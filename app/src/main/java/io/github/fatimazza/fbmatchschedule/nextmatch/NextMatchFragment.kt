e7d65b94088f7d46d650a92391d9875c7779187f
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-06 05:25:50 +0700

    Next Match Fragment - Implement swipe refresh

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/nextmatch/NextMatchFragment.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/nextmatch/NextMatchFragment.kt
@@ -2,6 +2,7 @@ package io.github.fatimazza.fbmatchschedule.nextmatch
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -17,7 +18,9 @@ import io.github.fatimazza.fbmatchschedule.network.ApiRepository
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.startActivity
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class NextMatchFragment: Fragment(), MatchView {
 
@@ -26,6 +29,8 @@ class NextMatchFragment: Fragment(), MatchView {
     private lateinit var adapter: MainMatchAdapter
 
     private lateinit var presenter: NextMatchPresenter
+
+    private lateinit var swipeRefresh: SwipeRefreshLayout
 
     private lateinit var listNextEvent: RecyclerView
 
@@ -38,9 +43,17 @@ class NextMatchFragment: Fragment(), MatchView {
     class NextMatchFragmentUI: AnkoComponent<NextMatchFragment> {
         override fun createView(ui: AnkoContext<NextMatchFragment>): View = with(ui) {
             frameLayout {
-                owner.listNextEvent = recyclerView {
-                    lparams(width = matchParent, height = wrapContent)
-                    layoutManager = LinearLayoutManager(ctx)
+
+                owner.swipeRefresh = swipeRefreshLayout {
+                    setColorSchemeResources(R.color.colorAccent,
+                            android.R.color.holo_green_light,
+                            android.R.color.holo_orange_light,
+                            android.R.color.holo_red_light)
+
+                    owner.listNextEvent = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
                 }
             }
         }
@@ -50,7 +63,14 @@ class NextMatchFragment: Fragment(), MatchView {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
         initPresenter()
+
+        swipeRefresh.isRefreshing = true
         requestEventData()
+
+        swipeRefresh.onRefresh {
+            events.clear()
+            requestEventData()
+        }
     }
 
     private fun initAdapter() {
@@ -69,6 +89,8 @@ class NextMatchFragment: Fragment(), MatchView {
     }
 
     override fun showEventList(data: List<Event>) {
+        swipeRefresh.isRefreshing = false
+
         events.clear()
         events.addAll(data)
         adapter.notifyDataSetChanged()
