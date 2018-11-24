e5dac9d3ce5ddf3fcf69a3d539c6ac970c61d899
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-24 23:26:53 +0700

    Unit Test - Prepare Context Provider for Unit Testing

---
+++ app/src/test/java/io/github/fatimazza/fbmatchschedule/util/TestContextProvider.kt
@@ -0,0 +1,8 @@
+package io.github.fatimazza.fbmatchschedule.util
+
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+class TestContextProvider : CoroutineContextProvider() {
+    override val main: CoroutineContext = Dispatchers.Unconfined
+}
