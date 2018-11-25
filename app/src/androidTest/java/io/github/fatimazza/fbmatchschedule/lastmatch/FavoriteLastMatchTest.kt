4bbfc597dd1011892f491d1c2780ddfd265184a5
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-25 10:40:21 +0700

    Instrumentation Test - Add Test for Favorite Last Match

---
+++ app/src/androidTest/java/io/github/fatimazza/fbmatchschedule/lastmatch/FavoriteLastMatchTest.kt
@@ -0,0 +1,70 @@
+package io.github.fatimazza.fbmatchschedule.lastmatch
+
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView
+import io.github.fatimazza.fbmatchschedule.R
+import io.github.fatimazza.fbmatchschedule.R.id.*
+import io.github.fatimazza.fbmatchschedule.main.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FavoriteLastMatchTest {
+
+    @Rule
+    @JvmField var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testAddLastMatchToFavorite() {
+
+        Espresso.onView(withId(bottom_navigation))
+                .check(matches(isDisplayed()))
+        Espresso.onView(withId(last_match))
+                .perform(click())
+
+        Thread.sleep(2000)
+
+        Espresso.onView(withId(last_match_list))
+                .check(matches(isDisplayed()))
+        Espresso.onView(withId(last_match_list))
+                .perform(RecyclerViewActions
+                        .scrollToPosition<RecyclerView.ViewHolder>(10))
+        Espresso.onView(withId(last_match_list))
+                .perform(RecyclerViewActions
+                        .actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click()))
+
+        Thread.sleep(2000)
+
+        Espresso.onView(withId(ll_team_detail))
+                .check(matches((isDisplayed())))
+
+        Espresso.onView(withId(add_to_favorite))
+                .check(matches(isDisplayed()))
+        Espresso.onView(withId(add_to_favorite))
+                .perform(click())
+
+        Espresso.onView(ViewMatchers.withText(R.string.favorite_added))
+                .check(matches(isDisplayed()))
+        Espresso.pressBack()
+
+        Espresso.onView(withId(bottom_navigation))
+                .check(matches(isDisplayed()))
+        Espresso.onView(withId(favorite_match))
+                .perform(click())
+
+        Thread.sleep(2000)
+
+        Espresso.onView(withId(favorite_list))
+                .check(matches(isDisplayed()))
+    }
+}
