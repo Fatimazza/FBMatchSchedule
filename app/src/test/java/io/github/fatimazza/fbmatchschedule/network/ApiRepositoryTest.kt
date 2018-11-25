e1322bc8bb7a0d75ec18efb632fed53e504723ad
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-25 07:43:46 +0700

    Unit Test - Add Test for Request API

---
+++ app/src/test/java/io/github/fatimazza/fbmatchschedule/network/ApiRepositoryTest.kt
@@ -0,0 +1,30 @@
+package io.github.fatimazza.fbmatchschedule.network
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+class ApiRepositoryTest {
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Mock
+    private
+    lateinit var apiRepository: ApiRepository
+
+    @Test
+    fun testDoRequest() {
+        apiRepository = mock(ApiRepository::class.java)
+        val url = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=German%20Bundesliga"
+
+        apiRepository.doRequest(url)
+        verify(apiRepository).doRequest(url)
+    }
+}
