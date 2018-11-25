f8bf57bdcb08a4806c049f95b2a5028c8badc624
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-25 08:50:24 +0700

    Unit Test - Add Test for Match Detail Presenter

---
+++ app/src/test/java/io/github/fatimazza/fbmatchschedule/matchdetail/MatchDetailPresenterTest.kt
@@ -0,0 +1,67 @@
+package io.github.fatimazza.fbmatchschedule.matchdetail
+
+import com.google.gson.Gson
+import io.github.fatimazza.fbmatchschedule.model.Team
+import io.github.fatimazza.fbmatchschedule.model.TeamResponse
+import io.github.fatimazza.fbmatchschedule.network.ApiRepository
+import io.github.fatimazza.fbmatchschedule.network.TheSportDBApi
+import io.github.fatimazza.fbmatchschedule.util.TestContextProvider
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+
+class MatchDetailPresenterTest {
+
+    @Mock
+    private
+    lateinit var view: MatchDetailActivity
+
+    @Mock
+    private
+    lateinit var apiRepository: ApiRepository
+
+    @Mock
+    private
+    lateinit var gson: Gson
+
+    private lateinit var presenter: MatchDetailPresenter
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        presenter = MatchDetailPresenter(view, apiRepository, gson, TestContextProvider())
+    }
+
+    @Test
+    fun testGetTeamDetail() {
+        val teams: MutableList<Team> = mutableListOf()
+        val homeTeamResponse = TeamResponse(teams)
+        val awayTeamResponse = TeamResponse(teams)
+        val teamHomeId = "133604"
+        val teamAwayId = "133616"
+
+        GlobalScope.launch {
+
+            `when`(gson.fromJson(
+                    apiRepository.doRequest(
+                            TheSportDBApi.getTeamDetail(teamHomeId)), TeamResponse::class.java))
+                    .thenReturn(homeTeamResponse)
+
+            `when`(gson.fromJson(
+                    apiRepository.doRequest(
+                            TheSportDBApi.getTeamDetail(teamAwayId)), TeamResponse::class.java))
+                    .thenReturn(awayTeamResponse)
+
+            presenter.getTeamDetail(teamHomeId, teamAwayId)
+
+            Mockito.verify(view).showHomeTeamDetail(homeTeamResponse.teams[0])
+            Mockito.verify(view).showAwayTeamDetail(awayTeamResponse.teams[0])
+        }
+    }
+}
