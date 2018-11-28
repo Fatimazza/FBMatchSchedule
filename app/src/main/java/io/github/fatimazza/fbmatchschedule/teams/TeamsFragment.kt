b54078806368ff97dcf60b6d0b044de7b1632deb
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-28 19:28:47 +0700

    Teams Fragment - Add layout with Anko

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/teams/TeamsFragment.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/teams/TeamsFragment.kt
@@ -2,15 +2,27 @@ package io.github.fatimazza.fbmatchschedule.teams
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.frameLayout
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
+import io.github.fatimazza.fbmatchschedule.R
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class TeamsFragment : Fragment() {
+
+    private lateinit var listTeam: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var spinner: Spinner
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
             : View? {
@@ -21,8 +33,35 @@ class TeamsFragment : Fragment() {
     class TeamsUI: AnkoComponent<TeamsFragment> {
         override fun createView(ui: AnkoContext<TeamsFragment>): View {
             return with(ui) {
-                frameLayout {
+                linearLayout {
+                    lparams(width = matchParent, height = wrapContent)
+                    orientation = LinearLayout.VERTICAL
+                    padding = dip(16)
 
+                    owner.spinner = spinner {
+                        id = R.id.spinner
+                    }
+                    owner.swipeRefresh = swipeRefreshLayout {
+                        setColorSchemeResources(R.color.colorAccent,
+                                android.R.color.holo_green_light,
+                                android.R.color.holo_orange_light,
+                                android.R.color.holo_red_light)
+
+                        relativeLayout {
+                            lparams(width = matchParent, height = wrapContent)
+
+                            owner.listTeam = recyclerView {
+                                id = R.id.list_team
+                                lparams(width = matchParent, height = wrapContent)
+                                layoutManager = LinearLayoutManager(ctx)
+                            }
+
+                            owner.progressBar = progressBar {
+                            }.lparams{
+                                centerHorizontally()
+                            }
+                        }
+                    }
                 }
             }
         }
