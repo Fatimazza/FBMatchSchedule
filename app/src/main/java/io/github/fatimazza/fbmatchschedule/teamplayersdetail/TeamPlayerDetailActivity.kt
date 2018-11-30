7b58334de41dd69cd335ac66078965b872da3321
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-30 18:36:34 +0700

    Team Player Detail - Add Anko Layout

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/teamplayersdetail/TeamPlayerDetailActivity.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/teamplayersdetail/TeamPlayerDetailActivity.kt
@@ -2,24 +2,119 @@ package io.github.fatimazza.fbmatchschedule.teamplayersdetail
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
+import android.view.Gravity
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
+import io.github.fatimazza.fbmatchschedule.R
 import io.github.fatimazza.fbmatchschedule.model.Players
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.linearLayout
-import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class TeamPlayerDetailActivity : AppCompatActivity(), TeamPlayerDetailView {
+
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var progressBar: ProgressBar
+
+    private lateinit var playerName: TextView
+    private lateinit var playerFanart: ImageView
+    private lateinit var playerDescription: TextView
+
+    private lateinit var playerWeight: TextView
+    private lateinit var playerHeight: TextView
+    private lateinit var playerPosition: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TeamPlayerDetailUI().setContentView(this)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.title = "Player Name"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     class TeamPlayerDetailUI: AnkoComponent<TeamPlayerDetailActivity> {
         override fun createView(ui: AnkoContext<TeamPlayerDetailActivity>): View {
             return with(ui) {
                 linearLayout {
+                    lparams(width = matchParent, height = wrapContent)
+                    orientation = LinearLayout.VERTICAL
+
+                    owner.swipeRefresh = swipeRefreshLayout {
+                        setColorSchemeResources(R.color.colorAccent,
+                                android.R.color.holo_green_light,
+                                android.R.color.holo_orange_light,
+                                android.R.color.holo_red_light)
+
+                        scrollView {
+                            isVerticalScrollBarEnabled = false
+                            relativeLayout {
+                                lparams(width = matchParent, height = wrapContent)
+
+                                linearLayout {
+                                    lparams(width = matchParent, height = wrapContent)
+                                    padding = dip(10)
+                                    orientation = LinearLayout.VERTICAL
+
+                                    owner.playerFanart = imageView() {
+                                        setBackgroundColor(resources.getColor(R.color.colorAccent))
+                                    }.lparams(width = matchParent, height = wrapContent)
+
+                                    owner.playerPosition = textView() {
+                                        topPadding = dip(8)
+                                        gravity = Gravity.CENTER_HORIZONTAL
+                                    }
+
+                                    linearLayout {
+                                        lparams(width = matchParent, height = wrapContent)
+                                        orientation = LinearLayout.HORIZONTAL
+                                        topPadding = dip(8)
+
+                                        textView {
+                                            text = resources.getString(R.string.player_weight)
+                                            gravity = Gravity.CENTER_HORIZONTAL
+                                        }.lparams(width = dip(0), height = wrapContent, weight = 1.0f)
+                                        textView {
+                                            text = resources.getString(R.string.player_height)
+                                            gravity = Gravity.CENTER_HORIZONTAL
+                                        }.lparams(width = dip(0), height = wrapContent, weight = 1.0f)
+                                    }
+
+                                    linearLayout {
+                                        lparams(width = matchParent, height = wrapContent)
+                                        orientation = LinearLayout.HORIZONTAL
+
+                                        owner.playerWeight = textView() {
+                                            textSize = 20f
+                                            gravity = Gravity.CENTER_HORIZONTAL
+                                        }.lparams(width = dip(0), height = wrapContent, weight = 1.0f)
+                                        owner.playerHeight = textView() {
+                                            textSize = 20f
+                                            gravity = Gravity.CENTER_HORIZONTAL
+                                        }.lparams(width = dip(0), height = wrapContent, weight = 1.0f)
+                                    }
+
+                                    owner.playerDescription = textView()
+                                            .lparams { margin = dip(8) }
+
+                                }.lparams{
+                                    centerHorizontally()
+                                }
+
+                                owner.progressBar = progressBar()
+                                        .lparams {
+                                    centerHorizontally()
+                                }
+
+                            }
+                        }
+                    }
 
                 }
             }
