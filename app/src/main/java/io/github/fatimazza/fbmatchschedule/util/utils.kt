17d2a6392e9ef1d5756bc0523ad05ba99c534d4d
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-12-02 12:55:40 +0700

    Utils - Add function to check Internet Connection availability

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/util/utils.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/util/utils.kt
@@ -1,5 +1,7 @@
 package io.github.fatimazza.fbmatchschedule.util
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.view.View
 import java.text.SimpleDateFormat
 import java.util.*
@@ -10,6 +12,12 @@ fun View.visible() {
 
 fun View.invisible() {
     visibility = View.INVISIBLE
+}
+
+fun isOnline(context: Context): Boolean {
+    val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val netInfo = cm.activeNetworkInfo
+    return netInfo != null && netInfo.isConnectedOrConnecting
 }
 
 fun formatToSimpleString(date: Date?): String? = with(date ?: Date()){
