bdee6ec26885a971854d9db9ad0994a480cf1c49
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-12-08 19:25:25 +0700

    Search Match - Add Up Navigation

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/searchmatch/SearchMatchActivity.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/searchmatch/SearchMatchActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.SearchView
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import io.github.fatimazza.fbmatchschedule.R
 import org.jetbrains.anko.AnkoComponent
@@ -16,6 +17,8 @@ class SearchMatchActivity : AppCompatActivity(), SearchView.OnQueryTextListener 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SearchMatchUI().setContentView(this)
+
+        setupActionBar()
     }
 
     class SearchMatchUI: AnkoComponent<SearchMatchActivity> {
@@ -25,6 +28,12 @@ class SearchMatchActivity : AppCompatActivity(), SearchView.OnQueryTextListener 
             }
         }
     }
+
+    private fun setupActionBar() {
+        supportActionBar?.title = getString(R.string.search_match)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.searchview_menu, menu)
@@ -43,5 +52,15 @@ class SearchMatchActivity : AppCompatActivity(), SearchView.OnQueryTextListener 
 
     override fun onQueryTextChange(newText: String?): Boolean {
         return false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
