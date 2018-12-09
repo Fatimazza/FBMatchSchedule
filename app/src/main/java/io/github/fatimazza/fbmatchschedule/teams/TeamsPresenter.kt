ced52205b8862fef51687d98c2170066bff5a30b
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-12-09 14:15:28 +0700

    Team Presenter - Change to Coroutine Scope instead of Global Scope

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/teams/TeamsPresenter.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/teams/TeamsPresenter.kt
@@ -6,59 +6,58 @@ import io.github.fatimazza.fbmatchschedule.model.TeamResponse
 import io.github.fatimazza.fbmatchschedule.network.ApiRepository
 import io.github.fatimazza.fbmatchschedule.network.TheSportDBApi
 import io.github.fatimazza.fbmatchschedule.util.CoroutineContextProvider
+import kotlinx.coroutines.*
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class TeamsPresenter(private val view: TeamsView,
                      private val apiRepository: ApiRepository,
                      private val gson: Gson,
-                     private val context: CoroutineContextProvider = CoroutineContextProvider()) {
+                     private val context: CoroutineContextProvider = CoroutineContextProvider())
+    : CoroutineScope {
+
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     fun getLeaguesList() {
-        view.showLoading()
-
-        GlobalScope.launch(context.main) {
-            val data = async(context.background) {
+        CoroutineScope(context.main).launch {
+            view.showLoading()
+            val data = withContext(context.background) {
                 gson.fromJson(apiRepository.doRequest(
                         TheSportDBApi.getAllLeagues()),
                         LeaguesResponse::class.java)
             }
-            view.showLeagueList(data.await().leagues)
+            view.showLeagueList(data.leagues)
             view.hideLoading()
         }
-
     }
 
     fun getTeamList(league: String?) {
-        view.showLoading()
-
-        GlobalScope.launch(context.main) {
-            val data = async(context.background) {
+        CoroutineScope(context.main).launch {
+            view.showLoading()
+            val data = withContext(context.background) {
                 gson.fromJson(apiRepository.doRequest(
                         TheSportDBApi.getTeams(league)),
                         TeamResponse::class.java)
             }
-            view.showTeamList(data.await().teams)
+            view.showTeamList(data.teams)
             view.hideLoading()
         }
-
     }
 
     fun searchTeam(teamName: String?) {
-        view.showLoading()
-
-        GlobalScope.launch(context.main) {
-            val data = async(context.background) {
+        CoroutineScope(context.main).launch {
+            view.showLoading()
+            val data = withContext(context.background) {
                 gson.fromJson(apiRepository.doRequest(
                         TheSportDBApi.searchTeam(teamName)),
                         TeamResponse::class.java)
             }
-            view.showTeamList(data.await().teams)
+            view.showTeamList(data.teams)
             view.hideLoading()
         }
-
     }
+
 }
 
