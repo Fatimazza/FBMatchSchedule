aaf62fc849616601d1e48009ecf2bc9ab4057972
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-12-09 15:38:44 +0700

    Team Players Presenter - Change to Coroutine Scope instead of Global Scope

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/teamplayers/TeamPlayersPresenter.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/teamplayers/TeamPlayersPresenter.kt
@@ -5,29 +5,33 @@ import io.github.fatimazza.fbmatchschedule.model.PlayerResponse
 import io.github.fatimazza.fbmatchschedule.network.ApiRepository
 import io.github.fatimazza.fbmatchschedule.network.TheSportDBApi
 import io.github.fatimazza.fbmatchschedule.util.CoroutineContextProvider
+import kotlinx.coroutines.*
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class TeamPlayersPresenter(private val view: TeamPlayersView,
                            private val apiRepository: ApiRepository,
                            private val gson: Gson,
-                           private val context: CoroutineContextProvider = CoroutineContextProvider()) {
+                           private val context: CoroutineContextProvider = CoroutineContextProvider())
+    : CoroutineScope {
+
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     fun getPlayersList(teamId: String?) {
-        view.showLoading()
 
-        GlobalScope.launch(context.main) {
-            val data = async(context.background) {
+        CoroutineScope(context.main).launch {
+            view.showLoading()
+            val data = withContext(context.background) {
                 gson.fromJson(apiRepository.doRequest(
                         TheSportDBApi.getPlayer(teamId)),
                         PlayerResponse::class.java)
             }
-            view.showPlayerList(data.await().player)
+            view.showPlayerList(data.player)
             view.hideLoading()
         }
-
+        
     }
 }
 
