3b3e7f7e96948dd7a38141ab4013b061f393f1d4
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-12-09 15:39:18 +0700

    Team Player Detail Presenter - Change to Coroutine Scope instead of Global Scope

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/teamplayersdetail/TeamPlayerDetailPresenter.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/teamplayersdetail/TeamPlayerDetailPresenter.kt
@@ -5,27 +5,30 @@ import io.github.fatimazza.fbmatchschedule.model.PlayerDetailResponse
 import io.github.fatimazza.fbmatchschedule.network.ApiRepository
 import io.github.fatimazza.fbmatchschedule.network.TheSportDBApi
 import io.github.fatimazza.fbmatchschedule.util.CoroutineContextProvider
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class TeamPlayerDetailPresenter(private val view: TeamPlayerDetailView,
                                 private val apiRepository: ApiRepository,
                                 private val gson: Gson,
-                                private val context: CoroutineContextProvider = CoroutineContextProvider()) {
+                                private val context: CoroutineContextProvider = CoroutineContextProvider())
+    : CoroutineScope {
+
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     fun getPlayerDetail(playerId: String) {
 
-        view.showLoading()
-
-        GlobalScope.launch(context.main){
-            val data = async {
+        CoroutineScope(context.main).launch {
+            view.showLoading()
+            val data = withContext(context.background) {
                 gson.fromJson(apiRepository
                         .doRequest(TheSportDBApi.getPlayerDetail(playerId)),
                         PlayerDetailResponse::class.java
                 )
             }
-            view.showPlayerDetail(data.await().players)
+            view.showPlayerDetail(data.players)
             view.hideLoading()
         }
 
