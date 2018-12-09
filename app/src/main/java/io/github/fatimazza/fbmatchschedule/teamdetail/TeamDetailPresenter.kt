79f0ef46bcfdf7b8577e7e4b3807458c05ff0075
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-12-09 14:35:49 +0700

    Team Detail Presenter - Change to Coroutine Scope instead of Global Scope

--- app/src/main/java/io/github/fatimazza/fbmatchschedule/teamdetail/TeamDetailPresenter.kt
+++ app/src/main/java/io/github/fatimazza/fbmatchschedule/teamdetail/TeamDetailPresenter.kt
@@ -6,27 +6,30 @@ import io.github.fatimazza.fbmatchschedule.network.ApiRepository
 import io.github.fatimazza.fbmatchschedule.network.TheSportDBApi
 import io.github.fatimazza.fbmatchschedule.teamoverview.TeamOverviewView
 import io.github.fatimazza.fbmatchschedule.util.CoroutineContextProvider
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class TeamDetailPresenter(private val view: TeamOverviewView,
                           private val apiRepository: ApiRepository,
                           private val gson: Gson,
-                          private val context: CoroutineContextProvider = CoroutineContextProvider()) {
+                          private val context: CoroutineContextProvider = CoroutineContextProvider())
+    : CoroutineScope {
+
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     fun getTeamDetail(teamId: String) {
 
-        view.showLoading()
-
-        GlobalScope.launch(context.main){
-            val data = async {
+        CoroutineScope(context.main).launch {
+            view.showLoading()
+            val data = withContext(context.background) {
                 gson.fromJson(apiRepository
                         .doRequest(TheSportDBApi.getTeamDetail(teamId)),
                         TeamResponse::class.java
                 )
             }
-            view.showTeamDetail(data.await().teams)
+            view.showTeamDetail(data.teams)
             view.hideLoading()
         }
 
